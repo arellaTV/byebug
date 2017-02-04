4e252d822ef540da9e746bd4474b7b6d5928796d
David Rodríguez <deivid.rodriguez@gmail.com>
2017-02-04 09:27:19 -0200

    Remove dynamic sample class & module names
    Since , we no longer need this. One argument for keeping it is to make
    parallelization of the test suite easier. I think, until we plan to do
    that, it's better to choose the added readability of not having it.

--- test/support/test_case.rb
+++ test/support/test_case.rb
@@ -54,8 +54,8 @@ module Byebug
     # Cleanup main Byebug namespace from dummy test classes and modules
     #
     def cleanup_namespace
-      force_remove_const(Byebug, example_class)
-      force_remove_const(Byebug, example_module)
+      force_remove_const(Byebug, 'ExampleClass')
+      force_remove_const(Byebug, 'ExampleModule')
     end
 
     #
@@ -73,37 +73,10 @@ module Byebug
     end
 
     #
-    # Fully namespaced example class
-    #
-    def example_full_class
-      "Byebug::#{example_class}"
-    end
-
-    #
-    # Name of the temporary test class
-    #
-    def example_class
-      "#{camelized_path}Class"
-    end
-
-    #
-    # Name of the temporary test module
-    #
-    def example_module
-      "#{camelized_path}Module"
-    end
-
-    #
     # Temporary folder where the test file lives
     #
     def example_folder
       @example_folder ||= Dir.tmpdir
-    end
-
-    private
-
-    def camelized_path
-      camelize(File.basename(example_path, '.rb'))
     end
   end
 end
